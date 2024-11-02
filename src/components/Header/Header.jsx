6afe153df3e8debd6e00b79cb06f35ea61559072
andrewzhivolup <ferari1190@mail.ru>
2024-11-02 21:57:06 +0300

    refactor: remove animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Header.scss';
 
 const Header = () => {
-    const [password, setPassword] = useState('Password');
-    const seconds = 3;
-
-    useEffect(() => {
-        setTimeout(() => {
-            setPassword(passwordTitleSwap());
-        }, seconds * 1000);
-    }, [password]);
-
-    /**
-   * Функция, которая перемешивает элементы массива titleList и возвращает случайный элемент массива.
-   * @return {string} Один из элементов массива titleList.
-   */
-    const passwordTitleSwap = () => {
-        const titleList = [
-            'Password',
-            'P@ssword',
-            'passWord',
-            'pAsSwOrD',
-            'PaSsWoRd',
-            'pASSword',
-            '=^.__.^=',
-        ];
-        titleList.sort(() => Math.random() - 0.5);
-        return password !== titleList[0] ? titleList[0] : titleList[1];
-    };
+    const version = process.env.REACT_APP_VERSION;
 
     return (
         <div className="header">
+            <h1 className="h1">password generator</h1>
             <h2 className="h2">v{version}</h2>
         </div>
     );
