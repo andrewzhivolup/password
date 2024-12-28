8409808da69dbcaab5e39ba40652536c91fec204
andrewzhivolup <ferari1190@mail.ru>
2024-12-28 12:48:32 +0300

    feat: added clear settings on version click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,17 @@ const Header = () => {
     const version = __APP_VERSION__;
 
     const { t } = useTranslation();
+
+    function clearSetting() {
+        localStorage.clear();
+        window.location.reload();
+    }
+
     return (
         <div className={cls.Header}>
             <h1 className={cls.title}>{t('генератор паролей')}</h1>
             <h2 className={cls.version}>
-                {t('версия')}: {version}
+                {t('версия')}: <span onClick={clearSetting}>{version}</span>
             </h2>
         </div>
     );
